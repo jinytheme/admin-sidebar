91b0879421af7376735a3a18c88fcf7f59d36006
hojinlee <infohojin@gmail.com>
2022-10-04 15:53:55 +0900

    first

---
+++ sidebar/brand.blade.php
@@ -0,0 +1,6 @@
+<a class="sidebar-brand" href="/_admin">
+    <span class="sidebar-brand-text align-middle">
+        JinyERP
+        <sup><small class="badge bg-primary text-uppercase">Admin</small></sup>
+    </span>
+</a>
